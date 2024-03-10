47828370ce9021d7005eb9fc918a1fbc6c27b0ad
Mariana Pereira <marianapereira.s@live.com>
2024-03-10 19:42:04 -0300

    Correções de lint - 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,13 +7,13 @@ def is_palindrome_recursive(word, low_index, high_index):
     if low_index >= high_index:
         if low_index == int(word_length / 2):
             return True
-        
+
         else:
             return False
 
     else:
-      if word[low_index] == word[high_index]:
-          return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-      
-      else:
-          return False
+        if word[low_index] == word[high_index]:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+        else:
+            return False
