eed01b0546958fb361b2b76cc4e3460febde882b
Mariana Pereira <marianapereira.s@live.com>
2024-03-10 19:40:48 -0300

    Requisitos 3.1 a 3.3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    word_length = len(word)
+
+    if word_length == 0:
+        return False
+
+    if low_index >= high_index:
+        if low_index == int(word_length / 2):
+            return True
+        
+        else:
+            return False
+
+    else:
+      if word[low_index] == word[high_index]:
+          return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+      
+      else:
+          return False
